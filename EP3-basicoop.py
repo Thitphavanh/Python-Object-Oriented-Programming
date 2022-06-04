659bb89eca6c9fc8de8fc20b47ef5a72a3e1dd4f
Hery <thitphavanh.psv@gmail.com>
2022-06-04 12:36:57 +0700

    update

--- EP3-basicoop.py
+++ EP3-basicoop.py
@@ -2,15 +2,22 @@
 class Employee:
 
     # create method
-    def detail(self, name, salary):
+    def detail(self, name, salary, department):
         self.name = name
         self.salary = salary
-        print('My Employee Infomartion = {}, {}'.format(self.name, self.salary))
+        self.department = department
+
+    def showData(self):
+        print('Employee Infomartion : name = {}, salary = {}, department = {} '.format(
+            self.name, self.salary, self.department))
 
 
 # create object
 emp1 = Employee()
-emp1.detail('Noy', 1500000)
+emp1.detail('Noy', 4500000, 'Engineer')
 
 emp2 = Employee()
-emp2.detail('Tick', 1200000)
+emp2.detail('Tick', 2500000, 'Programmer')
+
+emp1.showData()
+emp2.showData()
